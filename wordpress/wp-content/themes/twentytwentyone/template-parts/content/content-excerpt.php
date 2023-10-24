da5c189be1ba64652c3cd5d72194189021fe24bc
VanDanh <tranvandanh157@gmail.com>
2023-10-25 03:42:35 +0700

    module 5 demo

--- wordpress/wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
+++ wordpress/wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
@@ -12,15 +12,68 @@
  */
 
 ?>
+<?php
+$post = get_post();
+$date = $post->post_date;
+$day = date("j", strtotime($date));
+$month = date("m", strtotime($date));
+?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php get_template_part( 'template-parts/header/excerpt-header', get_post_format() ); ?>
+	<div class="container">
+		<div class="item-card">
+		<div class="row">
+			<div class="col-5">
+				<img class="w-100 h-100" src="https://media.istockphoto.com/id/1067347086/photo/cat-with-blue-eyes-looks-at-camera.jpg?s=612x612&w=0&k=20&c=UP1yQs6o0eidm4L2F74DDm02pyeH40MwBwwGRAKhb4E=" alt="">
+			</div>
+			<div class="col-1">
+				<div class="d-flex flex-column">
+					<div class="ngay">
+						<?= $day?>
+					</div>
+					<div class="thang">
+						<?= "Tháng" . $month?>
+					</div>
+				</div>
+			</div>
+			<div class="col-6">
+				<div class="entry-content">
+					<div class="post-title">
+					<?php get_template_part( 'template-parts/header/excerpt-header', get_post_format() ); ?>
+					</div>
+					<?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?>
+				</div><!-- .entry-content -->
+			</div>
+		</div>
+		</div>
+	</div>
 
-	<div class="entry-content">
-		<?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer default-max-width">
-		<?php twenty_twenty_one_entry_meta_footer(); ?>
-	</footer><!-- .entry-footer -->
+	<!-- <footer class="entry-footer default-max-width">
+	<?php twenty_twenty_one_entry_meta_footer(); ?>
+	</footer> .entry-footer  -->
 </article><!-- #post-${ID} -->
+
+<style>
+	.item-card{
+		box-shadow: 0px 0px 2px 2px lightgray;
+	}
+	.post-title a{
+		text-decoration: none !important;
+		color: #428BCA;
+	}
+	.entry-content{
+		border-left: 1px black solid;
+		height:80%;
+	}
+	.ngay{
+		font-size: 80px;
+		font-weight: 700;
+	}
+	.thang{
+		font-size:24px;
+		color:darkgray;
+	}
+	.ngay, .thang{
+		text-align:center;
+	}
+</style>
