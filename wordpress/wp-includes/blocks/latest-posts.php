a7e6147ea7830e14d1fb0c68073dbbb850dae219
Vietlam03 <21211tt1008@mail.tdc.edu.vn>
2023-10-25 01:29:02 +0700

    push module_10

--- wordpress/wp-includes/blocks/latest-posts.php
+++ wordpress/wp-includes/blocks/latest-posts.php
@@ -128,31 +128,7 @@ function render_block_core_latest_posts($attributes)
 	if (isset($attributes['displayFeaturedImage']) && $attributes['displayFeaturedImage']) {
 		update_post_thumbnail_cache($query);
 	}
-	/*
-	$query        = new WP_Query();
-                            $recent_posts = $query->query($args);
-
-                            foreach ($recent_posts as $post) {
-                                $post_link = esc_url(get_permalink(get_the_ID()));
-                                $title     = get_the_title($post);
-                                $date = get_the_date();
-                                $chuoi = esc_html(get_the_date(DATE_W3C));
-
-
-                                // Chuyển đổi chuỗi thành đối tượng datetime
-                                $datetime = date_create($chuoi);
-
-                                // Định dạng thời gian theo định dạng "d/m/Y"
-                                $ngay_thang_nam = $datetime->format('d/m/Y');
-
-                                // Tách chuỗi thành mảng
-                                $mang = explode("/", $ngay_thang_nam);
-
-                                // Gán giá trị cho các biến
-                                $ngay = $mang[0];
-                                $thang = $mang[1];
-                                $nam = $mang[2];
-	*/
+	
 	$list_items_markup = '';
 
 	foreach ($recent_posts as $post) {
